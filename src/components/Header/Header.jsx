0c011f2ea8ffaf5e4d47296ec7fff4b3d7137f38
NatashaKSS <xnagol@gmail.com>
2017-08-13 14:42:30 +0800

    EDIT styles for nav header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 import styles from './Header.css';
 
 const Header = () => {
-    return (
-      <header className={styles.header}>
-        <div className={styles.container}>
-          <h1 className={styles.title}>Fruits & Veggies</h1>
-          <nav>
-            <ul className={styles.navList}>
-              <li className={styles.navItem}><a href="#">Home</a></li>
-              <li className={styles.navItem}><a href="#">About</a></li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    );
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Fruits & Veggies</h1>
+        <nav>
+          <ul className={styles.navList}>
+            <li className={styles.navItem}><a href="#">Home</a></li>
+            <li className={styles.navItem}><a href="#">About</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
