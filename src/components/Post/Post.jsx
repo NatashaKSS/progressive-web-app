cd50b2478e63cfe66e5b3fc93a464fb6f8794dd7
NatashaKSS <xnagol@gmail.com>
2017-08-13 18:12:11 +0800

    EDITED to support routing to appropriate post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
+import PostsAPI from '../../api/PostsAPI.js';
+
 import styles from './Post.css';
 
-const Post = () => {
-  return (
-    <div>
-      <h2 className={styles.title}><a className={styles.link} href="#">Title</a></h2>
-      <p className={styles.content}>Some text here</p>
-    </div>
-  );
+const Post = (props) => {
+  const post = PostsAPI.getPostWithID(props.match.params.id);
+
+  if (!post) {
+    return <div>Sorry, the post with this ID was not found!</div>
+  } else {
+    return (
+      <div>
+        <h2 className={styles.title}>{post.title}</h2>
+        <p className={styles.content}>{post.content}</p>
+      </div>
+    );
+  }
 }
 
 export default Post;
--- src/components/PostCollection/PostCollection.jsx
+++ src/components/PostCollection/PostCollection.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Post from '../Post/Post.jsx';
+import PostSelection from '../PostSelection/PostSelection.jsx';
 
 import styles from './PostCollection.css';
 
 const PostCollection = ({children}) => {
   return (
     <div className={styles.pageContainer}>
-      <h2 className={styles.title}>My Collection of Healthy Food</h2>
+      <h2 className={styles.title}>My Collection of Healthy Foods</h2>
       <div className={styles.postsContainer}>
-        {children}
+        <Switch>
+          <Route exact path='/posts' component={PostSelection}/>
+          <Route path='/posts/:id' component={Post}/>
+        </Switch>
       </div>
     </div>
   );
