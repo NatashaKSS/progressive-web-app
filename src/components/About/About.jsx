ee781c9dd4c69267f43c116a4ddbc1ad99bbe530
NatashaKSS <xnagol@gmail.com>
2017-08-13 15:28:12 +0800

    ADDED About page component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styles from './About.css';
+
+const About = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>About</h1>
+      <p className={styles.content}>Blah blah blah</p>
+    </div>
+  );
+}
+
+export default About;
