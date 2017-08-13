ef4df3bec0c2805e8b635ccf82b82ec718a90b2a
NatashaKSS <xnagol@gmail.com>
2017-08-13 18:28:22 +0800

    ADDED BrowserHistory support for react router

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, BrowserHistory } from 'react-router-dom';
 
 import App from './components/App.jsx';
 
 render((
-  <BrowserRouter>
+  <BrowserRouter history={BrowserHistory}>
     <App />
   </BrowserRouter>
 
