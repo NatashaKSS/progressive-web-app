2510c12a0da95ba55ee8c43c716f50a4ac73538b
NatashaKSS <xnagol@gmail.com>
2017-08-18 01:40:41 +0800

    ADDED snapshot test for router

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import { MemoryRouter, BrowserHistory } from 'react-router-dom';
+
+import Home from '../Home/Home';
+import About from '../About/About';
+import PostCollection from '../PostCollection/PostCollection';
 import Main from './Main';
 
-test('<Main /> should render as expected', () => {
-  const component = shallow(<Main />);
-  const tree = toJson(component);
-  expect(tree).toMatchSnapshot();
+describe('<Main />', () => {
+  test('<Main /> should render as expected', () => {
+    const component = mount(
+      <MemoryRouter history={BrowserHistory}>
+        <Main />
+      </MemoryRouter>
+    );
+    const tree = toJson(component);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('<Main /> should render <Home /> correctly at route \'/\'', () => {
+    const component = mount(
+      <MemoryRouter initialEntries={[ '/' ]} initialIndex={0}>
+        <Main />
+      </MemoryRouter>
+    );
+    expect(component.find(Home).length).toBe(1);
+  });
+
+  test('<Main /> should render <About /> correctly at route \'/about\'', () => {
+    const component = mount(
+      <MemoryRouter initialEntries={[ '/about' ]} initialIndex={0}>
+        <Main />
+      </MemoryRouter>
+    );
+    expect(component.find(About).length).toBe(1);
+  });
+
+  test('<Main /> should render <PostCollection /> correctly at route \'/posts\'', () => {
+    const component = mount(
+      <MemoryRouter initialEntries={[ '/posts' ]} initialIndex={0}>
+        <Main />
+      </MemoryRouter>
+    );
+    expect(component.find(PostCollection).length).toBe(1);
+  });
+
 });
