bc81915419dbb3a0e8ca5c980879263f541af271
NatashaKSS <xnagol@gmail.com>
2017-08-18 02:32:05 +0800

    MADE Post test cover all outcomes

--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 
 import Post from './Post';
 
 describe('<Post />', () => {
   test('<Post /> should render as expected', () => {
     const component = shallow(<Post match={{ params: { id: "010" }}} />);
-    const tree = toJson(component);
-    expect(tree).toMatchSnapshot();
+    expect(component.find(Post));
   });
 
-  test('<Post /> should render ID not found as there is no post with this id', () => {
-    const component = shallow(<Post match={{ params: { id: "does_not_exist_001" }}} />);
-    const tree = toJson(component);
-    expect(tree).toMatchSnapshot();
+  test('<Post /> should render ID not found as props.match is null', () => {
+    const component = shallow(<Post match={ null } />);
+    expect(component.contains("Sorry, the post with this ID was not found!")).toBe(true);
+  });
+
+  test('<Post /> should render ID not found as props.match.params is null', () => {
+    const component = shallow(<Post match={{ params: null }} />);
+    expect(component.contains("Sorry, the post with this ID was not found!")).toBe(true);
   });
 
   test('<Post /> should render ID not found if no post retrieved', () => {
     const component = shallow(<Post match={{ params: { id: null }}} />);
-    const tree = toJson(component);
-    expect(tree).toMatchSnapshot();
+    expect(component.contains("Sorry, the post with this ID was not found!")).toBe(true);
   })
+
+  test('<Post /> should render ID not found as there is no post with this id', () => {
+    const component = shallow(<Post match={{ params: { id: "does_not_exist_001" }}} />);
+    expect(component.contains("Sorry, the post with this ID was not found!")).toBe(true);
+  });
+
 });
