3d757f20cbbcee7fcce928475ea47417a3c0aac5
NatashaKSS <xnagol@gmail.com>
2017-08-18 02:09:08 +0800

    ADDED tests for a Post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -5,7 +5,8 @@ import PostsAPI from '../../api/PostsAPI.js';
 import styles from './Post.css';
 
 const Post = (props) => {
-  const post = PostsAPI.getPostWithID(props.match.params.id);
+  const id = getPostIDFromProps(props);
+  const post = id ? PostsAPI.getPostWithID(id) : null;
 
   if (!post) {
     return <div>Sorry, the post with this ID was not found!</div>
@@ -17,6 +18,23 @@ const Post = (props) => {
       </div>
     );
   }
+}
+
+/**
+ * Obtains the post id from props. Checks if props and nested objects are
+ * valid or not, otherwise, an id with a null value will be returned.
+ *
+ * @param  {Object} props React props
+ * @return {String}       String representation of the ID of a post
+ */
+const getPostIDFromProps = (props) => {
+  let id = null;
+  if (props.match) {
+    if (props.match.params) {
+      id = props.match.params.id;
+    }
+  }
+  return id;
 }
 
 export default Post;
--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
+
 import Post from './Post';
 
-test('<Post /> should render as expected', () => {
-  const component = shallow(<Post match={{ params: { id: "mock_001" }}} />);
-  const tree = toJson(component);
-  expect(tree).toMatchSnapshot();
+describe('<Post />', () => {
+  test('<Post /> should render as expected', () => {
+    const component = shallow(<Post match={{ params: { id: "010" }}} />);
+    const tree = toJson(component);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('<Post /> should render ID not found as there is no post with this id', () => {
+    const component = shallow(<Post match={{ params: { id: "does_not_exist_001" }}} />);
+    const tree = toJson(component);
+    expect(tree).toMatchSnapshot();
+  });
+
+  test('<Post /> should render ID not found if no post retrieved', () => {
+    const component = shallow(<Post match={{ params: { id: null }}} />);
+    const tree = toJson(component);
+    expect(tree).toMatchSnapshot();
+  })
 });
